55b8d814edf58ef0527c65689f92ad58e5e1dafd
Bethwel Kiplagat <sjemnyango9@gmail.com>
2025-05-31 12:34:43 +0300

    Entry file

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import TaskList from './components/TaskList';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-50 py-12 px-4">
+      <TaskList />
+    </div>
+  );
+}
